3a13c4c4e2b13bdac607f3b464792e4a93981346
Andy Marthin <mr.andymarthin@gmail.com>
2017-08-28 15:40:12 +0700

    convert register to kotlin

---
+++ app/src/main/java/com/example/mcspicy/belajarkotlin/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mcspicy.belajarkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
